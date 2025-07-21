d9af59510f442cc5ade7b94fbe34e7be4a838d56
Seidu <abdulsalamseidu@protonmail.com>
2025-07-21 22:54:28 +0100

    set app structure

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite dev server
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
