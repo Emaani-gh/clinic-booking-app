32ed69ac7bb7c2f2f52ef2c2de9af9f98e1b269c
Seidu Abdul Salam <90971780+Emaani-gh@users.noreply.github.com>
2025-07-27 15:09:39 +0300

    Set up authentication system: register, login, JWT, and middleware (#2)

---
+++ backend/controller/registerUser.js
@@ -0,0 +1,33 @@
+const User = require("../models/User");
+const asyncHandler = require("../utils/asyncHandler");
+const genToken = require("../utils/genToken");
+const bcrypt = require("bcryptjs");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { Fname, Lname, email, password, role } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exist" });
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    Fname,
+    Lname,
+    email,
+    password: hashedPass,
+    role,
+  });
+
+  const token = genToken(newUser.id);
+
+  res.status(201).json({
+    message: "User created successfuly",
+    user: { Fname, Lname, email, role },
+    token,
+  });
+});
+
+module.exports = registerUser;
