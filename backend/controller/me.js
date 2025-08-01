1058ab738aed715f02f16406bf6ebaf14689f94f
Seidu <abdulsalamseidu@protonmail.com>
2025-08-01 19:56:06 +0300

    wrap /me controller with async handler

--- backend/controller/me.js
+++ backend/controller/me.js
@@ -1,6 +1,7 @@
-const getMe = (req, res) => {
-  const user = req.user;
-  res.status(200).json(user);
-};
+const asyncHandler = require("../utils/asyncHandler");
 
+const getMe = asyncHandler(async (req, res) => {
+  const user = await req.user;
+  res.status(200).json(user);
+});
 module.exports = getMe;
